182829e5fc41515dc36053e0b49f2129e261be32
Greg Mazenko <bizcorr9586@att.net>
2020-06-25 19:38:51 -0500

    enabled surefire plugin for maven

--- src/test/java/BJackUnitTest.java
+++ src/test/java/BJackUnitTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BJackUnitTests {
+public class BJackUnitTest {
 
     @Test
     void testFaceCount(){
